60690b03a3d9204e7b55feb53b4de7bde44cb529
Fabian Jütte <fabian.juette@tu-clausthal.de>
2017-01-03 11:47:37 +0100

    Mein Zwischenstand

--- oodb_vs_documentstores/Juette_Berger/build.sh
+++ oodb_vs_documentstores/Juette_Berger/build.sh
@@ -1,2 +1,1 @@
-pandoc --toc *.md -o Ausarbeitung.pdf
-pandoc --toc *.md -o Ausarbeitung.docx
+pandoc --bibliography=quellen.bib -cls=din-1505-2-numeric-alphabetical.csl --toc -V lang=de -o Ausarbeitung.pdf Ausarbeitung.md
