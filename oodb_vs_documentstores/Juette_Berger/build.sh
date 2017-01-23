daf769b0dbf82fb6e456e3720d8e95a978257acf
FJuette <Fabian.Juette@tu-clausthal.de>
2017-01-23 13:07:55 +0100

    Build Script

--- oodb_vs_documentstores/Juette_Berger/build.sh
+++ oodb_vs_documentstores/Juette_Berger/build.sh
@@ -1,1 +1,3 @@
-pandoc --bibliography=quellen.bib -cls=din-1505-2-numeric-alphabetical.csl --toc -V lang=de Kapitel/*.md -o Ausarbeitung.docx
+cd Kapitel
+pandoc --bibliography=../quellen.bib -cls=../din-1505-2-numeric-alphabetical.csl --toc -V lang=de *.md -o ../Ausarbeitung.docx
+cd ..
