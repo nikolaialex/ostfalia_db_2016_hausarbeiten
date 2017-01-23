7fff6901bffd7e5104b1f45cccb666a4089c09de
FJuette <Fabian.Juette@tu-clausthal.de>
2017-01-23 10:00:40 +0100

    Anpassung von Links und Quellen

--- oodb_vs_documentstores/Juette_Berger/build.sh
+++ oodb_vs_documentstores/Juette_Berger/build.sh
@@ -1,1 +1,1 @@
-pandoc --bibliography=quellen.bib -cls=din-1505-2-numeric-alphabetical.csl --toc -V lang=de kapitel/*.md -o Ausarbeitung.pdf
+pandoc --bibliography=quellen.bib -cls=din-1505-2-numeric-alphabetical.csl --toc -V lang=de Kapitel/*.md -o Ausarbeitung.docx
